39ec6e23973f0fed38deaccd64ae7f3d454852f8
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-07-25 17:18:46 +0300

    first commit

---
+++ lua/whoami_packs/actors.lua
@@ -0,0 +1,143 @@
+--[[
+
+Author: tochonement
+Email: tochonement@gmail.com
+
+24.07.2021
+
+--]]
+
+local CATEGORY = "Actors"
+
+if CLIENT then
+   whoi.lang.addPhrase("russian", "Actors", "Актёры")
+end
+
+--[[
+    whoi.word.register("goblin")
+    :SetCategory(CATEGORY)
+    :SetName("Goblin") -- Name for card must be in English
+    :SetImageUrl("https://i.imgur.com/7GIIezt.png")
+    :Translate("russian", "Goblin")
+    :Translate("german", "Goblin")
+    :Translate("french", "Goblin")
+    :Translate("italian", "Goblin")
+]]
+
+-- Males
+
+whoi.word.register("tom_hanks")
+:SetCategory(CATEGORY)
+:SetName("Tom Hanks")
+:SetImageUrl("https://i.imgur.com/eCyJ2r4.png")
+:Translate("russian", "Том Хэнкс")
+
+whoi.word.register("morgan_freeman")
+:SetCategory(CATEGORY)
+:SetName("Morgan Freeman")
+:SetImageUrl("https://i.imgur.com/YXajqCy.png")
+:Translate("russian", "Морган Фримен")
+
+whoi.word.register("harrison_ford")
+:SetCategory(CATEGORY)
+:SetName("Harrison Ford")
+:SetImageUrl("https://i.imgur.com/3X6k95x.jpg")
+:Translate("russian", "Харрисон Форд")
+
+whoi.word.register("jackie_chan")
+:SetCategory(CATEGORY)
+:SetName("Jackie Chan")
+:SetImageUrl("https://i.imgur.com/3X6k95x.jpg")
+:Translate("russian", "Джеки Чан")
+
+whoi.word.register("dwayne_johnson")
+:SetCategory(CATEGORY)
+:SetName("Dwayne Johnson")
+:SetImageUrl("https://i.imgur.com/oodKngi.png")
+:Translate("russian", "Дуэйн Джонсон")
+
+whoi.word.register("will_smith")
+:SetCategory(CATEGORY)
+:SetName("Will Smith")
+:SetImageUrl("https://i.imgur.com/ySeewQM.png")
+:Translate("russian", "Уилл Смит")
+
+whoi.word.register("keeanu_reeves")
+:SetCategory(CATEGORY)
+:SetName("Keanu Reeves")
+:SetImageUrl("https://i.imgur.com/2NboYQP.png")
+:Translate("russian", "Киану Ривз")
+
+whoi.word.register("brad_pitt")
+:SetCategory(CATEGORY)
+:SetName("Brad Pitt")
+:SetImageUrl("https://i.imgur.com/YY8W5ma.png")
+:Translate("russian", "Брэд Питт")
+
+whoi.word.register("johnny_depp")
+:SetCategory(CATEGORY)
+:SetName("Johnny Depp")
+:SetImageUrl("https://i.imgur.com/Op77gJW.png")
+:Translate("russian", "Джонни Дэпп")
+
+whoi.word.register("leonardo_dicaprio")
+:SetCategory(CATEGORY)
+:SetName("Leonardo DiCaprio")
+:SetImageUrl("https://i.imgur.com/BaZGILK.png")
+:Translate("russian", "Леонардо Ди Каприо")
+
+whoi.word.register("tom_cruise")
+:SetCategory(CATEGORY)
+:SetName("Tom Cruise")
+:SetImageUrl("https://i.imgur.com/I01E6Ug.png")
+:Translate("russian", "Том Круз")
+
+-- Females
+
+whoi.word.register("scarlett_johansson")
+:SetCategory(CATEGORY)
+:SetName("Scarlett Johansson")
+:SetImageUrl("https://i.imgur.com/E4mB20H.png")
+:Translate("russian", "Скарлетт Йоханссон")
+
+whoi.word.register("natalie_portman")
+:SetCategory(CATEGORY)
+:SetName("Natalie Portman")
+:SetImageUrl("https://i.imgur.com/DUBh2Pe.png")
+:Translate("russian", "Натали Портман")
+
+whoi.word.register("megan_fox")
+:SetCategory(CATEGORY)
+:SetName("Megan Fox")
+:SetImageUrl("https://i.imgur.com/W0KaEEx.png")
+:Translate("russian", "Меган Фокс")
+
+whoi.word.register("emma_watson")
+:SetCategory(CATEGORY)
+:SetName("Emma Watson")
+:SetImageUrl("https://i.imgur.com/CDLNq9y.png")
+:Translate("russian", "Эмма Уотсон")
+
+whoi.word.register("angelina_jolie")
+:SetCategory(CATEGORY)
+:SetName("Angelina Jolie")
+:SetImageUrl("https://i.imgur.com/W9uocX2.jpeg")
+:Translate("russian", "Анджелина Джоли")
+
+whoi.word.register("jennifer_aniston")
+:SetCategory(CATEGORY)
+:SetName("Jennifer Aniston")
+:SetImageUrl("https://i.imgur.com/TGOfUZd.png")
+:Translate("russian", "Дженнифер Энистон")
+
+whoi.word.register("emma_stone")
+:SetCategory(CATEGORY)
+:SetName("Emma Stone")
+:SetImageUrl("https://i.imgur.com/yPuleMf.png")
+:Translate("russian", "Эмма Стоун")
+
+whoi.word.register("emilia_clark")
+:SetCategory(CATEGORY)
+:SetName("Emilia Clark")
+:SetImageUrl("https://i.imgur.com/rwZR0Qs.png")
+:Translate("russian", "Эмилия Кларк")
